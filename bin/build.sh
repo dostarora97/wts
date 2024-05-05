9e31e240e3096eb53b93f51f5c560306c63ca19f
I533118 <dost.arora@sap.com>
2024-05-06 03:03:14 +0530

    bin changes

--- bin/build.sh
+++ bin/build.sh
@@ -1,11 +1,10 @@
+#!/bin/bash
+
 #install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # install python using brew
 brew install python
-
-# Install git using homebrew
-brew install git
 
 # Make virtual environment
 python3 -m venv wts-venv
@@ -14,8 +13,13 @@ source wts-venv/bin/activate
 # Install necessary modules
 python3 -m pip install flask pandas pyinstaller
 
+# Get app.py
+curl -fsSL https://raw.githubusercontent.com/dostarora97/wts/main/app.py > ./app.py
+
 # Build app executable
 pyinstaller --onefile app.py
 
 # Make sure port is closed
 sudo lsof -ti:8000 | xargs sudo kill -9
+
+echo "Executable created in some-path(todo)"

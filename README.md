66bfc40351337edd6f8dd8871675a6d95d5d251d
I533118 <dost.arora@sap.com>
2024-05-06 06:18:18 +0530

    Improve init script

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # wts
 
 1. `chmod +x ./bin/build.sh`
-2. `source ./bin/build.sh`
-3. Open Platypus App on Mac and install cmd line. Platypus Menu > Settings > Install Command Line
+2. `chmod +x ./bin/create-app.sh`
+3. `chmod +x ./bin/clean.sh`
+4. `source ./bin/build.sh`
+5. Open Platypus App on Mac and install cmd line. Platypus Menu > Settings > Install Command Line
 ![Platypus install cmd](assets/platypus-settings.png)
-4. `chmod +x ./bin/create-app.sh`
-5. `./bin/create-app.sh`
-6. `chmod +x ./bin/clean.sh`
+6. `./bin/create-app.sh`
 7. `./bin/clean.sh`
--- bin/init.sh
+++ bin/init.sh
@@ -1,1 +1,33 @@
-echo "init"
+#!/bin/bash
+
+echo "Initializing"
+
+# Install Homebrew if not already installed
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Homebrew is already installed."
+fi
+
+# Install Git using Homebrew if not already installed
+if ! command -v git &> /dev/null; then
+  echo "Installing Git..."
+  brew install git
+else
+  echo "Git is already installed."
+fi
+
+git clone https://github.com/dostarora97/wts.git
+
+cd wts || exit
+
+chmod +x ./bin/build.sh
+chmod +x ./bin/create-app.sh
+chmod +x ./bin/clean.sh
+
+source ./bin/build.sh
+
+echo "Steps 1 to 4 completed. Continue from Step 5 in README.md"
+
+open README.md

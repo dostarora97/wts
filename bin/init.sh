ebff96eba578db496d98a841097f6d6d61c7ad59
I533118 <dost.arora@sap.com>
2024-05-06 22:12:23 +0530

    Improve init script

--- bin/init.sh
+++ bin/init.sh
@@ -6,7 +6,7 @@ echo "Initializing..."
 if ! command -v brew &> /dev/null; then
   echo "Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  echo "eval $(/opt/homebrew/bin/brew shellenv)" >> ~/.zprofile
   eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Homebrew is already installed."
